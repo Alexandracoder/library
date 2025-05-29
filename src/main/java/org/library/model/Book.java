49f79d08ad4d2ff2c09959fcc0811af482a4f1fa
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-05-29 13:59:58 +0200

    feat(book): add getter, setters and constructor method

--- src/main/java/org/library/model/Book.java
+++ src/main/java/org/library/model/Book.java
@@ -1,4 +1,72 @@
 package org.library.model;
 
 public class Book {
+    private int id;
+    private String title;
+    private String authors;
+    private String description;
+    private String isbn;
+    private String genre;
+    private int year;
+
+    public Book(String title, String authors, String description, String isbn, String genre, int year) {
+        this.title = title;
+        this.authors = authors;
+        this.description = description;
+        this.isbn = isbn;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
