3afc0c6e93f35a7152b2b520557c8250280643cf
mary kenny <marykenny2704@yahoo.com>
2025-05-30 16:46:58 +0200

    fix(dbmanager):dependency in pom,update dbmanager
    # Conflicts:
    #	src/main/java/org/library/Main.java

--- src/main/java/org/library/Main.java
+++ src/main/java/org/library/Main.java
@@ -1,13 +1,9 @@
 package org.library;
 
-
 import org.library.config.DBManager;
-
-import static org.library.config.DBManager.initConnection;
 
 public class Main {
     public static void main(String[] args) {
         DBManager.initConnection();
-
     }
 }
