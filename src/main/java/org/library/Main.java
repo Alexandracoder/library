e61950961a7bef9766c3320c36792941ded47f8a
mary kenny <marykenny2704@yahoo.com>
2025-05-30 16:20:26 +0200

    feat(dbmanager): add iinitConnection and closeConnection methods

--- src/main/java/org/library/Main.java
+++ src/main/java/org/library/Main.java
@@ -1,11 +1,13 @@
 package org.library;
 
 
+import org.library.config.DBManager;
+
 import static org.library.config.DBManager.initConnection;
 
 public class Main {
     public static void main(String[] args) {
-        initConnection();
+        DBManager.initConnection();
 
     }
 }
