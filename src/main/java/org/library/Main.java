e8f965273156378f718884b49d1c143bd6fba092
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-06-02 15:38:32 +0200

    feat(bookcontroller): add bookController, saveBookController, findAllControler and call saveBookView and showBooks on Main

--- src/main/java/org/library/Main.java
+++ src/main/java/org/library/Main.java
@@ -1,9 +1,15 @@
 package org.library;
 
-import org.library.config.DBManager;
+import org.library.controller.BookController;
+import org.library.repository.BookRepository;
+import org.library.view.BookView;
 
 public class Main {
     public static void main(String[] args) {
-        DBManager.initConnection();
+        BookRepository bookRepository = new BookRepository();
+        BookController bookController = new BookController(bookRepository);
+        BookView bookView = new BookView(bookController);
+        bookView.saveBookView();
+        bookView.showBooks();
     }
 }
